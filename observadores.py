2ecad3b31028fef055b53f4b775b76418d687c3b
weliton <welingtonzsilva@gmail.com>
2022-03-21 22:02:03 -0300

    classe e métodos implementados

--- observadores.py
+++ observadores.py
@@ -1,10 +1,11 @@
 # -*- coding: UTF-8 -*-
-
-def imprime(nota_fiscal):
-    print('Imprimindo nota fiscal %s'%(nota_fiscal.cnpj))
-    
-def envia_por_email(nota_fiscal):
-    print('enviando nota fiscal por email %s'%(nota_fiscal.cnpj))
+class Observer(object):
         
-def salva_no_banco(nota_fiscal):
-    print('Salvando nota fiscal %s'%(nota_fiscal.cnpj))
+    def imprime(self,nota_fiscal):
+        print('Imprimindo nota fiscal %s'%(nota_fiscal.cnpj))
+        
+    def envia_por_email(self,nota_fiscal):
+        print('enviando nota fiscal por email %s'%(nota_fiscal.cnpj))
+            
+    def salva_no_banco(self,nota_fiscal):
+        print('Salvando nota fiscal %s'%(nota_fiscal.cnpj))
